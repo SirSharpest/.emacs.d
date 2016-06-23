1ad3d80a327b3dc59c2737eaba33fccb73690918
sirsharpest <nathan1hughes@gmail.com>
2016-06-23 16:36:04 +0100

    Added a lot of python stuff

--- init.el
+++ init.el
@@ -3,15 +3,16 @@
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 
-
 ;;Actually load packages required! 
 (package-initialize)
-
 
 ;;loading theme stuff
 ;;(load-theme 'zenburn t)
 (load-theme 'leuven t)      
 
+;;Turning on ido-mode as it seems to be needed by a few things 
+;;(require 'ido)
+;;(ido-mode t)
 
 ;;Helm stuff with some gtags
 (setq
@@ -22,6 +23,9 @@
  helm-gtags-prefix-key "\C-cg"
  helm-gtags-suggested-key-mapping t
  )
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse) 
 
 (require 'helm-gtags)
 ;; Enable helm-gtags-mode
@@ -39,6 +43,23 @@
 (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
 ;;End of helm stuff 
 
+;;turn on some saving data
+(savehist-mode 1)
+
+;;Indent mode
+;;(add-hook 'highlight-indentation-mode-hook 'python-mode)
+;;(set-face-background 'highlight-indentation-face "#e3e3d3")
+;;(set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+
+;;Fill column indicator
+;;(require 'fill-column-indicator)
+;;(define-globalized-minor-mode
+ ;;global-fci-mode fci-mode (lambda () (fci-mode 1)))
+;;(global-fci-mode t)
+
+;;Enable line numbers 
+(add-hook 'prog-mode-hook 'linum-mode)
+
 ;;Adding some company auto complete stuff
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
@@ -47,6 +68,9 @@
 (require 'flycheck)
 (global-flycheck-mode)
 
+;;Adding more python stuff
+(elpy-enable)
+
 (add-hook 'after-init-hook 'global-company-mode)
 
 (defun my/python-mode-hook ()
@@ -54,6 +78,7 @@
 
 (add-hook 'python-mode-hook 'my/python-mode-hook)
 (add-hook 'python-mode-hook 'run-python-internal);Auto-completion package
+
 
 ;;Turn on the golden ratio addon
 (require 'golden-ratio)
@@ -74,7 +99,6 @@
 (setq TeX-save-query nil)
 ;;(setq TeX-PDF-mode t)
 
-
 ;;Adding some flymake stuff 
 (require 'flymake)
 
@@ -94,7 +118,6 @@
 ;;Adding outline mode defs
 (defun turn-on-outline-minor-mode ()
 (outline-minor-mode 1))
-
 (add-hook 'LaTeX-mode-hook 'turn-on-outline-minor-mode)
 (add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
 (setq outline-minor-mode-prefix "\C-c \C-o") ; Or something else
@@ -142,22 +165,62 @@ i.e. change right window to bottom, or change bottom window to right."
 
 ;;Adding some custom keybinds
 
+;;Change the buffer list to be helm
+(global-set-key (kbd "C-b") 'helm-buffers-list)
+
 ;;Autocomplete
 (global-set-key (kbd "C-'") 'company-complete)
+
+
 (custom-set-variables
-
-
-
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-safe-themes
+   (quote
+    ("1dd4a70e719e4ad7b3581dbe18b50fd9b7147447ba87f59dc47627bf4856be8c" "40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" default)))
+ '(fci-rule-color "#383838")
+ '(hl-sexp-background-color "#efebe9")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (auctex zenburn-theme python-mode markdown-mode leuven-theme helm-projectile helm-gtags golden-ratio flyspell-correct company-jedi auto-complete))))
+    (elpy github-theme highlight-indentation fill-column-indicator ## auctex zenburn-theme python-mode markdown-mode leuven-theme helm-projectile helm-gtags golden-ratio flyspell-correct company-jedi auto-complete)))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
