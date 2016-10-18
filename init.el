395a105bcce993cf69c58d7c297d69167374dab7
Nathan <nathan1hughes@gmail.com>
2016-10-18 13:43:55 +0100

    Fixed bugs for newer versions of emacs

--- init.el
+++ init.el
@@ -35,7 +35,6 @@
     sr-speedbar
     flycheck
     yasnippet
-    puml-mode
     magit
     platformio-mode
     projectile
@@ -57,6 +56,8 @@
 
 ;; GENERAL SETUP
 ;; --------------------------------------
+
+(require 'helm)
 
 ;;(shell) ;; I like to turn on the shell when I startup!
 ;; Just aside note, eshell isn't great for completion
@@ -164,8 +165,24 @@
   (find-file  (concat "~/.emacs.d/org/daily/" (format-time-string "%Y-%m-%d.org" ))))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(comint-completion-addsuffix t)
+ '(comint-completion-autolist t)
+ '(comint-input-ignoredups t)
+ '(comint-move-point-for-output t)
+ '(comint-scroll-show-maximum-output t)
+ '(comint-scroll-to-bottom-on-input t)
+ '(flycheck-c/c++-gcc-executable nil)
+ '(org-agenda-files (list org-directory))
  '(org-directory "~/.emacs.d/org/daily")
- '(org-agenda-files (list org-directory)))
+ '(package-selected-packages
+   (quote
+    (magit puml-mode sr-speedbar flycheck-pyflakes py-autopep8 jedi company-jedi company-ycm company-web company-auctex company-arduino auctex markdown-mode golden-ratio alect-themes nyan-mode elpy)))
+ '(python-check-command "flake8")
+ '(python-shell-interpreter "python3"))
 
 ;;Helm SETUP;;
 ;;-------------------------------------------
@@ -367,36 +384,15 @@
      (yas-global-mode 0)
      ))
 
-(custom-set-variables
- '(comint-scroll-to-bottom-on-input t)  ; always insert at the bottom
- '(comint-scroll-to-bottom-on-output t) ; always add output at the bottom
- '(comint-scroll-show-maximum-output t) ; scroll to show max possible output
- '(comint-completion-autolist t)        ; show completion list when ambiguous
- '(comint-input-ignoredups t)           ; no duplicates in command history
- '(comint-completion-addsuffix t)       ; insert space/slash after file completion
- )
 
-					; interpret and use ansi color codes in shell output windows
+
+;; interpret and use ansi color codes in shell output windows
 (ansi-color-for-comint-mode-on)
 
-					; make completion buffers disappear after 3 seconds.
+;; make completion buffers disappear after 3 seconds.
 (add-hook 'completion-setup-hook
 	  (lambda () (run-at-time 3 nil
 				  (lambda () (delete-windows-on "*Completions*")))))
-
-
-;; run a few shells.
-(shell "*shell5*")
-(shell "*shell6*")
-(shell "*shell7*")
-
-					; C-5, 6, 7 to switch to shells
-(global-set-key [(control \5)]
-		(lambda () (interactive) (switch-to-buffer "*shell5*")))
-(global-set-key [(control \6)]
-		(lambda () (interactive) (switch-to-buffer "*shell6*")))
-(global-set-key [(control \7)]
-		(lambda () (interactive) (switch-to-buffer "*shell7*")))
 
 
 ;; Octave setup (THIS SHOULD BE DEFAULT PLEASE FIX IN EMACS 25>)
@@ -421,17 +417,7 @@
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(flycheck-c/c++-gcc-executable nil)
- '(package-selected-packages
-   (quote
-    (magit puml-mode sr-speedbar flycheck-pyflakes py-autopep8 jedi company-jedi company-ycm company-web company-auctex company-arduino auctex markdown-mode golden-ratio alect-themes nyan-mode elpy)))
- '(python-check-command "flake8")
- '(python-shell-interpreter "python3"))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
